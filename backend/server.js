378d62dc87b6f73bf3cc3905bc04785f36b46747
rv.dthought@gmail.com <rv.dthought@gmail.com>
2025-09-15 22:53:49 +0530

    removed cookies

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 
 
 app.use(express.json());
