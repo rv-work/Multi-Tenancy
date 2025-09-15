2d6ba54563c0d5518566645883ce443008e15fb5
rv.dthought@gmail.com <rv.dthought@gmail.com>
2025-09-15 22:55:32 +0530

    removed cookies

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "*", 
+    origin: ["https://multi-tenancy-omega.vercel.app" ,
+            "https://multi-tenancy-omega.vercel.app/" ,
+              "*"], 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
