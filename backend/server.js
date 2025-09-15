6d722a27b6e3f1fac60f4f88c6ee25228d6c4844
rv.dthought@gmail.com <rv.dthought@gmail.com>
2025-09-15 23:39:19 +0530

    Final

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     'https://multi-tenancy-omega.vercel.app', 
+    'https://multi-tenancy-omega.vercel.app/', 
     'http://localhost:5173', 
     '*' 
   ],
