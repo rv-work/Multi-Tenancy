7e25679c1811a59519c7dd28eaf59d981afdbd66
rv.dthought@gmail.com <rv.dthought@gmail.com>
2025-09-15 22:33:06 +0530

    Frontend URL

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: "https://multi-tenancy-omega.vercel.app",
-  credentials: true
-}));
+app.use(cors());
+
+
 app.use(express.json());
 app.use(cookieParser());
 
